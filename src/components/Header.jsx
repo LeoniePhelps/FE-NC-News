d1d54b2ae320c5661505d1a345b492641e952b9c
leoniephelps <leoniephelps96@gmail.com>
2022-08-22 21:08:51 +0100

    Add title heading

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+  return (
+    <>
+      <Link to="/">b</Link>
+      <h1>NC News</h1>
+    </>
+  );
+};
