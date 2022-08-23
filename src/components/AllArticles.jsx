625c87f0f4c69a30fd11474d7db50cf595e058ab
leoniephelps <leoniephelps96@gmail.com>
2022-08-23 13:07:46 +0100

    add get articles by topic functionality

--- src/components/AllArticles.jsx
+++ src/components/AllArticles.jsx
@@ -1,32 +1,38 @@
 import { React, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { getAllArticles } from "./api";
 import { ArticleCard } from "./ArticleCard";
+import { NavBar } from "./NavBar";
 
 export const AllArticles = () => {
   const [articles, setArticles] = useState([]);
+  const { slug } = useParams();
 
   useEffect(() => {
-    getAllArticles().then(({ data: { articles } }) => {
+    getAllArticles(slug).then(({ data: { articles } }) => {
       setArticles(articles);
     });
-  }, []);
+  }, [slug]);
 
   return (
-    <section className="all-articles">
-      {articles.map((article) => {
-        return (
-          <ArticleCard
-            key={article.article_id}
-            article_id={article.article_id}
-            author={article.author_id}
-            comment_count={article.comment_count}
-            created_at={article.created_at}
-            title={article.title}
-            topic={article.topic}
-            votes={article.votes}
-          />
-        );
-      })}
-    </section>
+    <>
+      <NavBar />
+      <section className="all-articles">
+        {articles.map((article) => {
+          return (
+            <ArticleCard
+              key={article.article_id}
+              article_id={article.article_id}
+              author={article.author_id}
+              comment_count={article.comment_count}
+              created_at={article.created_at}
+              title={article.title}
+              topic={article.topic}
+              votes={article.votes}
+            />
+          );
+        })}
+      </section>
+    </>
   );
 };
--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
-export const getAllArticles = () => {
+export const getAllArticles = (slug) => {
+  const topicStr = slug ? `?topic=${slug}` : ``;
   return axios.get(
-    "https://leonie-phelps-nc-news-be.herokuapp.com/api/articles"
+    `https://leonie-phelps-nc-news-be.herokuapp.com/api/articles${topicStr}`
   );
+};
+
+export const getTopics = () => {
+  return axios.get("https://leonie-phelps-nc-news-be.herokuapp.com/api/topics");
 };
