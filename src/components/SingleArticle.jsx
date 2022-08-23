fd3cb1c2a8d6c8be072ed477f13da609625dac8d
leoniephelps <leoniephelps96@gmail.com>
2022-08-23 21:34:37 +0100

    add functionality for single item

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getArticleById } from "../api";
 
 export const SingleArticle = () => {
+  const [singleArticle, setSingleArticle] = useState({});
+  const { article_id } = useParams();
+
+  useEffect(() => {
+    getArticleById(article_id).then(({ data }) => {
+      setSingleArticle(data);
+    });
+  }, [article_id]);
+
   return (
-    <div>SingleArticle</div>
-  )
-}
+    <div>
+      <h2>{singleArticle.title}</h2>
+      <p>{singleArticle.author}</p>
+      <p>{singleArticle.body}</p>
+      <p>{singleArticle.created_at}</p>
+      <p>{singleArticle.topic}</p>
+    </div>
+  );
+};
