72ea99c4f12a3e087685817cedca7dbd0beb3926
leoniephelps <leoniephelps96@gmail.com>
2022-08-24 13:42:50 +0100

    add use state for votes

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,26 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getArticleById } from "../api";
+import { Votes } from "./Votes";
 
 export const SingleArticle = () => {
-  const [singleArticle, setSingleArticle] = useState({});
+    const [singleArticle, setSingleArticle] = useState({});
+     const [optimisticVotes, setOptimisticVotes] = useState();
   const { article_id } = useParams();
 
   useEffect(() => {
     getArticleById(article_id).then(({ data }) => {
-      setSingleArticle(data);
+        setSingleArticle(data);
+        setOptimisticVotes(data.votes)
     });
   }, [article_id]);
 
   return (
-    <section className="single-article">
-      <h2 className="single-article-title">{singleArticle.title}</h2>
-      <p className="single-article-author">{singleArticle.author}</p>
-      <p className="single-article-body">{singleArticle.body}</p>
-      <section className="topic-and-date">
-        <p>{singleArticle.created_at}</p>
-        <p>{singleArticle.topic}</p>
+    <div>
+      <section className="single-article">
+        <h2 className="single-article-title">{singleArticle.title}</h2>
+        <p className="single-article-author">{singleArticle.author}</p>
+        <p className="single-article-body">{singleArticle.body}</p>
+        <section className="topic-and-date">
+          <p>{singleArticle.created_at}</p>
+          <p>{singleArticle.topic}</p>
+        </section>
       </section>
-    </section>
+          <Votes article_id={article_id} votes={singleArticle.votes} optimisticVotes={optimisticVotes} setOptimisticVotes={setOptimisticVotes } />
+    </div>
   );
 };
