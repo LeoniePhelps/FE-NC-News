8a4418e213a58656b17d5c27e78870052fcccfe8
leoniephelps <leoniephelps96@gmail.com>
2022-08-24 09:06:38 +0100

    add styling for readability for single article

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -13,12 +13,14 @@ export const SingleArticle = () => {
   }, [article_id]);
 
   return (
-    <div>
-      <h2>{singleArticle.title}</h2>
-      <p>{singleArticle.author}</p>
-      <p>{singleArticle.body}</p>
-      <p>{singleArticle.created_at}</p>
-      <p>{singleArticle.topic}</p>
-    </div>
+    <section className="single-article">
+      <h2 className="single-article-title">{singleArticle.title}</h2>
+      <p className="single-article-author">{singleArticle.author}</p>
+      <p className="single-article-body">{singleArticle.body}</p>
+      <section className="topic-and-date">
+        <p>{singleArticle.created_at}</p>
+        <p>{singleArticle.topic}</p>
+      </section>
+    </section>
   );
 };
