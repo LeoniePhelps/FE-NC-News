ad63ab92a0b427ce0bce40427b385467872428df
leoniephelps <leoniephelps96@gmail.com>
2022-08-26 11:55:14 +0100

    add functionality to sort by and order by

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,13 +2,22 @@ import { React, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getTopics } from "../api";
 
-export const NavBar = () => {
+export const NavBar = ({ setSortBy, setOrder }) => {
   const [topicList, setTopicList] = useState([]);
   useEffect(() => {
     getTopics().then(({ data: { topics } }) => {
       setTopicList(topics);
     });
   }, []);
+
+  const handleSortChange = (event) => {
+    setSortBy(event.target.value);
+  };
+
+  const handleOrderChange = (event) => {
+    setOrder(event.target.value);
+  };
+
   return (
     <section>
       <section>
@@ -30,6 +39,19 @@ export const NavBar = () => {
           );
         })}
       </nav>
+      <section>
+        <label> sort by...</label>
+        <select onChange={handleSortChange}>
+          <option value="created_at">created at</option>
+          <option value="votes">votes</option>
+          <option value="title">title</option>
+        </select>
+        <label> order by...</label>
+        <select onChange={handleOrderChange}>
+          <option value="DESC">descending</option>
+          <option value="ASC">ascending</option>
+        </select>
+      </section>
     </section>
   );
 };
